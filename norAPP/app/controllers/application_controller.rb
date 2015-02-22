b2e7e2d99fd496f24f78217218ce710ecb4e5181
Thomas Minshull <thomasminshull@gmail.com>
2015-02-22 15:49:01 -0400

    minor updates to css to modify flash has for user/index

--- norAPP/app/controllers/application_controller.rb
+++ norAPP/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def confirm_logged_in
     unless session[:user_id]
-      flash[:notice] = "Please log in."
+      flash[:notice] = "Please log in"
       redirect_to(:controller => 'users', :action => 'index')
       return false #haults function
     else 
